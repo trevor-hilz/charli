868778fd310be5d37a4dbec069703e61d40875f6
Trevor <trevorhilz@gmail.com>
2025-01-05 23:01:38 -0600

    added array

---
+++ client/src/utils/EventIntput.tsx
@@ -0,0 +1,11 @@
+const events = [
+  'Focal Aware',
+  'Focal Impaired',
+  'Focal to Bilateral Tonic-Clonic',
+  'Tonic-Clonic (Grand Mal)',
+  'Tonic',
+  'Clonic',
+  'Absence (Petit Mal)',
+  'Atonic',
+  'Myoclonic',
+];
