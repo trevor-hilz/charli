b2af5cc72713a07c409fe464894f339648e4e21a
Trevor <trevorhilz@gmail.com>
2025-01-09 21:23:00 -0600

    initial structure of input event implemented. Needs refactor

--- client/src/components/patient/InputEvent.tsx
+++ client/src/components/patient/InputEvent.tsx
@@ -1,13 +1,32 @@
+import React, {useState} from 'react';
+import Seizures from '../../utils/EventInput';
+import DatePicker from 'react-datepicker';
+
+import "react-datepicker/dist/react-datepicker.css";
+
 const InputEvent = () => {
+  const [startDate, setStartDate] = useState<Date | null> (new Date());
+
   return (
     <div className='patientDisplay'>
-      <button onClick={() => 'myFunction()'} className='dropbtn'>
-        Dropdown
-      </button>
-      <div id='dropdown' className='dropdown-content'>
-        <a>Tonic Clonic</a>
-        <a>Absent</a>
-      </div>
+      <select id='eventOption'>
+        <option value='' disabled selected>
+          Select an Event
+        </option>
+        {Seizures.map((seizure, index) => (
+                <option key={index} value={seizure}>
+                  {seizure}
+                </option>
+              ))}
+      </select>
+      <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+        <div>
+          Duration
+        <input type='form' placeholder='Minutes'></input>
+        <input type='form' placeholder='Seconds'></input>
+        </div>
+      <div>Emergency Drug Administered</div>
+      <input type='checkbox'></input>
     </div>
   );
 };
